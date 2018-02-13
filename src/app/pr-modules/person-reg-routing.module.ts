fbb7725d33ec17e6af2fe5f6d103db15ad3bf7c3
Rene Ricardo <rene.ricardo@datys.cu>
2018-02-13 22:04:02 +0100

    Setting the route for 'pr-main-page' component in person-reg-routing.module

--- src/app/pr-modules/person-reg-routing.module.ts
+++ src/app/pr-modules/person-reg-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { PrMainPageComponent } from './pr-main-page/pr-main-page.component';
+
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
-    // path: 'home', component: MyComponent
+    path: 'home', component: PrMainPageComponent
     // , children: [
     //   { path: ':id', component: OtherComponent }
     // ]
