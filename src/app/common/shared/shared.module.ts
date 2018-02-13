b48d9f2144a38b9e09f1650ff4e0f0298dd1db43
Rene Ricardo <rene.ricardo@datys.cu>
2018-02-13 21:45:08 +0100

    Creating the empty component 'pr-navbar'

--- src/app/common/shared/shared.module.ts
+++ src/app/common/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { ListComponent } from "./complements/pr-list/pr-list.component";
+import { PrNavbarComponent } from './layouts/pr-navbar/pr-navbar.component';
 
 //--
 const complements = [
@@ -13,7 +14,7 @@ const complements = [
 @NgModule({
     imports: [RouterModule, FormsModule,CommonModule],
     exports: [...complements],
-    declarations: [...complements],
+    declarations: [...complements, PrNavbarComponent],
     providers: []
 })
 
