0515cecf9adbc007707a97b0e6db2d455309d235
Rene Ricardo <rene.ricardo@datys.cu>
2018-02-13 23:03:19 +0100

    Adding some funcionality to component 'pr-main-page'

--- src/app/pr-modules/pr-main-page/pr-main-page.component.ts
+++ src/app/pr-modules/pr-main-page/pr-main-page.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Registry } from '../../common/interfaces/registry';
+import { PersonDataService } from '../../common/core/services/person-data/person-data.service';
+
 @Component({
   selector: 'pr-main-page',
   templateUrl: './pr-main-page.component.html',
   styleUrls: ['./pr-main-page.component.css']
 })
 export class PrMainPageComponent implements OnInit {
+
+  titles = ['Nombre', 'Hora'];
+  entries: Registry[];
 
   constructor() { }
 
