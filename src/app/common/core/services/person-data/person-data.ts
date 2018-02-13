c5468c905bbc31a1e8a5b718d7900fcc1dd28c77
Rene Ricardo <rene.ricardo@datys.cu>
2018-02-13 23:04:20 +0100

    Creating a mock file for person's data

---
+++ src/app/common/core/services/person-data/person-data.ts
@@ -0,0 +1,20 @@
+import { Person } from '../../../interfaces/person';
+
+export const PERSON_DATA: Person[] = [
+    {
+        name: 'Rene Ricardo Perez',
+        signature: 'rrp930302'
+    },
+    {
+        name: 'Viannys Doimeadios Fuentes',
+        signature: 'vdf920707'
+    },
+    {
+        name: 'Elisabeth Perez Ramos',
+        signature: 'epr700131'
+    },
+    {
+        name: 'Brian Alfonso Perez',
+        signature: 'bap060925'
+    },
+]
