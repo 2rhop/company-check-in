d52561e8bd422432998fba140e6d634e8f28ee4a
Rene Ricardo <rene.ricardo@datys.cu>
2018-02-13 22:03:19 +0100

    Creating the empty component 'pr-main-page'

--- src/app/pr-modules/person-reg.module.ts
+++ src/app/pr-modules/person-reg.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 
 import { PersonRegRoutingModule } from './person-reg-routing.module';
 import { SharedModule } from '../common/shared/shared.module';
+import { PrMainPageComponent } from './pr-main-page/pr-main-page.component';
 
 @NgModule({
     imports: [
         PersonRegRoutingModule,
         SharedModule
     ],
-    declarations: [],
+    declarations: [PrMainPageComponent],
 })
 export class PersonRegModule { }
---
+++ src/app/pr-modules/pr-main-page/pr-main-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pr-main-page',
+  templateUrl: './pr-main-page.component.html',
+  styleUrls: ['./pr-main-page.component.css']
+})
+export class PrMainPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
