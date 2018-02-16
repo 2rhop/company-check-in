248c8815baf1d6a949fc7051a36ead6fe4c8cb28
Rene Ricardo <i2rhop@gmail.com>
2018-02-16 09:31:46 -0500

    Adding the logic to the component 'pr-sign-form'

--- src/app/common/shared/complements/pr-sign-form/pr-sign-form.component.ts
+++ src/app/common/shared/complements/pr-sign-form/pr-sign-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ClassField } from '@angular/compiler/src/output/output_ast';
+import { Person } from '../../../interfaces/person';
 
 @Component({
   selector: 'pr-sign-form',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrSignFormComponent implements OnInit {
 
+  @Output() text_field = new EventEmitter();
+  invalid: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.init();
+  }
+
+  onSubmit(t: NgForm) {
+    var text: string = t.value.name;
+    (this.isValid(text) ? this.submit(text) : this.setInvalidField());
+  }
+
+  isValid(t: string): boolean {
+    let len: number = t.length;
+    return len == 9;
+  }
+
+  setInvalidField() {
+    this.invalid = 'invalid';
+  }
+
+  submit(t) {
+    this.text_field.emit(t)
+    this.init();
+  }
+
+  init() {
+    this.invalid = undefined;
   }
 
 }
