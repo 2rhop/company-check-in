8784988c3cd172f9ef8a617f930a1087bbec3a0c
Rene Ricardo <i2rhop@gmail.com>
2018-02-23 12:33:20 -0500

    Setting the color to timer when the time is over

--- src/app/person-reg/main/pr-main-page/pr-main-page.component.ts
+++ src/app/person-reg/main/pr-main-page/pr-main-page.component.ts
@@ -23,9 +23,13 @@ export class PrMainPageComponent implements OnInit, OnDestroy {
   personList: Person[];
   msg: string;
   title: string;
-  class: string; time: Date;
+  class: string; time: Date; timer_color: string;
   _subscribe_persons: Subscription;
   _subscribe_registries: Subscription;
+
+  _timeStart: Date;
+  _timeEnd: Date;
+
 
   constructor(private person_service: PersonDataService,
     private reg_service: RegistryDataService,
@@ -33,35 +37,49 @@ export class PrMainPageComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.init();
+    this.setDateRange();
+  }
+
+  setDateRange(): void {
+    this._timeStart = new Date('Fri Feb 23 2018 11:09:30 GMT-0500 (Hora estándar del Este)"')
+    this._timeEnd = new Date(Date.now());
+    this._timeStart.setHours(8, 15, 0);
+    this._timeEnd.setHours(16, 30, 0);
   }
 
   handlePersonRegistry(p) {
-    this._subscribe_persons = this.person_service.getPersonFromKey(p).subscribe(name => {
-      if (name != VALUE) {
-        this.title = 'Welcome: ';
-        this.msg = name;
-        this.class = 'success';
-        this.reg_service.addRegistry({
-          name: name,
-          time: this.time.toString()
-        }).subscribe(res => {
-          this.init();
-          console.log('response: ' + res.status);
-        },
-          error => {
-            console.log('error: ' + error);
-          });
-      } else {
-        this.msg = '';
-        this.title = 'Incorrect Person'.toUpperCase();
-        this.class = 'danger';
-      }
-    });
+    if (this.canSign()) {
+      this._subscribe_persons = this.person_service.getPersonFromKey(p).subscribe(name => {
+        if (name != VALUE) {
+          this.title = 'Welcome: ';
+          this.msg = name;
+          this.class = 'success';
+          this.reg_service.addRegistry({
+            name: name,
+            time: this.time.toString()
+          }).subscribe(res => {
+            this.init();
+          },
+            error => {
+              console.log('error: ' + error);
+            });
+        } else {
+          this.msg = '';
+          this.title = 'Incorrect Person'.toUpperCase();
+          this.class = 'danger';
+        }
+      });
+    }
 
   }
 
   getTimer(t: Date) {
     this.time = t;
+    if (this.canSign()) {
+      this.timer_color = 'success';
+    } else {
+      this.timer_color = 'danger';
+    }
   }
 
   init() {
@@ -76,5 +94,8 @@ export class PrMainPageComponent implements OnInit, OnDestroy {
     this._subscribe_persons.unsubscribe();
   }
 
+  canSign(): boolean {
+    return (this.time < this._timeStart) || (this.time > this._timeEnd);
+  }
 
 }
