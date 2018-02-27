99cf563abaa5776eeec7eb311ac6c7f7a097ebd7
Rene Ricardo <i2rhop@gmail.com>
2018-02-27 09:56:37 -0500

    Setting the messages into 'pr-main-page' component

--- src/app/person-reg/main/pr-main-page/pr-main-page.component.ts
+++ src/app/person-reg/main/pr-main-page/pr-main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewContainerRef } from '@angular/core';
 
 import { Registry } from '../../../common/interfaces/registry';
 import { Person } from '../../../common/interfaces/person';
@@ -8,6 +8,9 @@ import { PersonDataService, VALUE } from '../../../common/core/services/person-d
 import { RegistryDataService } from '../../../common/core/services/reg-data/registry-data.service';
 import { Subscription } from 'rxjs';
 import { ReversePipe } from '../../../common/core/pipes/reverse.pipe';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+import { MessageService } from '../../../common/core/services/messages/message.service';
+import { CustomToastOptions } from '../../../common/core/services/messages/custom-toast-options';
 
 @Component({
   selector: 'pr-main-page',
@@ -21,9 +24,7 @@ export class PrMainPageComponent implements OnInit, OnDestroy {
   titles = ['Nombre', 'Hora'];
   entries: Registry[];
   personList: Person[];
-  msg: string;
-  title: string;
-  class: string; time: Date; timer_color: string;
+  time: Date; timer_color: string;
   _subscribe_persons: Subscription;
   _subscribe_registries: Subscription;
 
@@ -33,7 +34,8 @@ export class PrMainPageComponent implements OnInit, OnDestroy {
 
   constructor(private person_service: PersonDataService,
     private reg_service: RegistryDataService,
-    private reverse: ReversePipe) { }
+    private reverse: ReversePipe, public toast_service: MessageService, private vcr: ViewContainerRef) {
+  }
 
   ngOnInit() {
     this.init();
@@ -41,7 +43,7 @@ export class PrMainPageComponent implements OnInit, OnDestroy {
   }
 
   setDateRange(): void {
-    this._timeStart = new Date('Fri Feb 23 2018 11:09:30 GMT-0500 (Hora estándar del Este)"')
+    this._timeStart = new Date(Date.now())
     this._timeEnd = new Date(Date.now());
     this._timeStart.setHours(8, 15, 0);
     this._timeEnd.setHours(16, 30, 0);
@@ -51,26 +53,24 @@ export class PrMainPageComponent implements OnInit, OnDestroy {
     if (this.canSign()) {
       this._subscribe_persons = this.person_service.getPersonFromKey(p).subscribe(name => {
         if (name != VALUE) {
-          this.title = 'Welcome: ';
-          this.msg = name;
-          this.class = 'success';
           this.reg_service.addRegistry({
             name: name,
             time: this.time.toString()
           }).subscribe(res => {
             this.init();
+            this.toast_service.showSuccess('Welcome: ' + name.toUpperCase(), this.vcr);
           },
             error => {
               console.log('error: ' + error);
             });
         } else {
-          this.msg = '';
-          this.title = 'Incorrect Person'.toUpperCase();
-          this.class = 'danger';
+          this.toast_service.showError('Incorrect Person', this.vcr);
         }
       });
+    } else {
+      this.toast_service.showInfo('You can not sign in this momment', this.vcr,
+        <CustomToastOptions>{ showCloseButton: true });
     }
-
   }
 
   getTimer(t: Date) {
